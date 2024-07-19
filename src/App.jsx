e5ebc83ab0a9255d6456deb1f88a6239951f661c
mcasto-09 <mgcastorani@gmail.com>
2024-07-18 21:34:29 -0600

    skeleton for html/css mostly done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
+ 
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="font-display flex justify-between p-3 text-xl ">
+        <p>Discover</p> 
+        <p>Login | Create Account</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <div className="flex justify-end pr-20">
+        Login | Create Account
+      </div> */}
+      <div className='font-display text-center pt-72 text-4xl pb-4'>
+        <h1>The Music Database </h1>
+      </div> 
+      <div className="font-display text-center text-xl border-solid border-2 w-20rem" > //use 20~rem
+        <input type="text" placeholder="search for something good" name="search"></input>
+    </div>
     </div>
   );
 }
