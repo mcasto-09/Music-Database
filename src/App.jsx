bcbc4f29b86116f618ed11fb51e875d4003189f6
mcasto-09 <mgcastorani@gmail.com>
2024-07-23 21:17:49 -0600

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ function App() {
       <div className='font-display text-center pt-72 text-4xl pb-4'>
         <h1>The Music Database </h1>
       </div> 
-      <div className="font-display text-center text-xl border-solid border-2 w-20rem" > //use 20~rem
-        <input type="text" placeholder="search for something good" name="search"></input>
+      <div className="flex justify-center font-display text-center text-lg border-solid border-2 w-[25rem] mx-auto" > 
+        <input className="w-full" type="text" placeholder="search" name="search"></input>
     </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import "./index.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+// import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom';
 import App from "./App";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/'></Route>
+  )
+)
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
