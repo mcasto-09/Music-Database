59ab8a2dde8b2007ee6b47dce72c10513ccf40b7
mcasto-09 <mgcastorani@gmail.com>
2024-07-27 14:32:01 -0600

    started server

--- server/main.js
+++ server/main.js
@@ -1,12 +1,22 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import session from 'express-session'
 
 const app = express();
+const port = '8000'
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'))
+app.use(session({
+  secret: 'shh',
+  saveUninitialized: true,
+  resave: false
+}))
 
 app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on http://localhost:3000"),
+ViteExpress.listen(app, 8000, () =>
+  console.log("Server is listening on http://localhost:8000"),
 );
